ddde7287daafdf4653ea3c1136eb2d0033fab903
NESH <146263730+nesh-hue@users.noreply.github.com>
2023-10-16 10:21:53 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-int main(void){
-   int a, b, c, d;
-   printf("Enter the first value:");
-   scanf("%d",&a);
-   printf("Enter the second value:");
-   scanf("%d",&b);
-   printf("Enter the third value:");
-   scanf("%d",&c);
-   d=a+b+c;
-   printf("%d+%d+%d=%d\n",a,b,c,d);
-   return 0;
+int main(void)
+{
+    float a =0;
+
+    while (a <=200 )
+
+    {
+        printf("%6.2f distance mi.=%6.2f distance km\n",
+               a,(a)*1.609344);
+        a=a+10;
+    }
+    return 0;
 }
-
-
